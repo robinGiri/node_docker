4fb3bda4a03eba6ce9f9f9a63246303d972cb4da
robinGiri <girirobin2018@gmail.com>
2021-10-01 17:26:32 +0545

    feat: mongoBD in docker with connect function

---
+++ config/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  MONGO_IP: process.env.MONGO_IP,
+  MONGO_PORT: process.env.MONGO_PORT,
+  MONGO_USER: process.env.MONGO_USER,
+  MONGO_PASSWORD: process.env.MONGO_PASSWORD
+}
--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const mongoose = require('mongoose');
+const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require('./config/config');
 
 const app = express()
 
@@ -7,5 +9,17 @@ const port = process.env.PORT || 3000
 app.get('/', (req, res) => {
   res.send('<h2>Namaskar, Welcome to node express Docker</h2>')
 })
+
+const reconnectWithMongoDB = () => {
+  mongoose
+  .connect(`mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`)
+  .then(() => console.log('DB connected ...'))
+  .catch((e) => {
+    console.log(e)
+    setTimeout(reconnectWithMongoDB, 5000)
+  })
+}
+
+reconnectWithMongoDB()
 
 app.listen(port, () => console.log(`we are at port ${port}`))
